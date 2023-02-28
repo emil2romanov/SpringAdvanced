093f5ed4e5f49ed8b71b8a6503be4e7b7cf25bc7
emil2romanov <eromanov8@gmail.com>
2023-02-28 14:17:52 +0200

    fixed

--- security-demo/src/main/java/bg/softuni/security/config/SecurityConfiguration.java
+++ security-demo/src/main/java/bg/softuni/security/config/SecurityConfiguration.java
@@ -18,20 +18,31 @@ public class SecurityConfiguration {
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
     http.
-        // defines which pages will be authorized
-        authorizeHttpRequests().
-          requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll().
-          requestMatchers("/", "/users/login", "/users/register", "/users/login-error").permitAll().
-          requestMatchers("/pages/moderators").hasRole(UserRoleEnum.MODERATOR.name()).
-          requestMatchers("/pages/admins").hasRole(UserRoleEnum.ADMIN.name()).
-        anyRequest().authenticated().
-          and().
-          formLogin().
+            // defines which pages will be authorized
+                    authorizeHttpRequests().
+            // allow access to all static files (images, CSS, js)
+                    requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll().
+            // the URL-s below are available for all users - logged in and anonymous
+                    requestMatchers("/", "/users/login", "/users/register", "/users/login-error").permitAll().
+            // only for moderators
+                    requestMatchers("/pages/moderators").hasRole(UserRoleEnum.MODERATOR.name()).
+            // only for admins
+                    requestMatchers("/pages/admins").hasRole(UserRoleEnum.ADMIN.name()).
+            anyRequest().authenticated().
+            and().
+            // configure login with HTML form
+                    formLogin().
             loginPage("/users/login").
-            usernameParameter(UsernamePasswordAuthenticationFilter.SPRING_SECURITY_FORM_USERNAME_KEY).
+            // the names of the user name, password input fields in the custom login form
+                    usernameParameter(UsernamePasswordAuthenticationFilter.SPRING_SECURITY_FORM_USERNAME_KEY).
             passwordParameter(UsernamePasswordAuthenticationFilter.SPRING_SECURITY_FORM_PASSWORD_KEY).
-        defaultSuccessUrl("/", true).
-        failureForwardUrl("/users/login-error");
+            // where do we go after login
+                    defaultSuccessUrl("/").//use true argument if you always want to go there, otherwise go to previous page
+            failureForwardUrl("/users/login-error").
+            and().logout().//configure logout
+            logoutUrl("/users/logout").
+            logoutSuccessUrl("/").//go to homepage after logout
+            invalidateHttpSession(true);
 
     return http.build();
   }
